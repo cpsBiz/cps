5d2b4db5f2647c3997591def8dc0b815bed162c0
최상혁 <shchoi3@enliple.com>
2024-06-18 14:45:12 +0900

    - 키우기

--- raising/src/main/java/com/mobcomms/raising/controller/UserController.java
+++ raising/src/main/java/com/mobcomms/raising/controller/UserController.java
@@ -17,18 +17,14 @@ public class UserController {
     public ResponseEntity<ApiResponse> signUp() {
         return ResponseEntity.ok(ApiResponse.ok());
     }
-
-     // 탈퇴
-     public ResponseEntity<ApiResponse> withdrawal() {
+    // 탈퇴
+    public ResponseEntity<ApiResponse> withdrawal() {
          return ResponseEntity.ok(ApiResponse.ok());
      }
-
     // 사용자캐릭터 변경 api
     public ResponseEntity<ApiResponse> updateUserCharacter() {
         return ResponseEntity.ok(ApiResponse.ok());
     }
-
-
     // 사용자 게임 상품 선택 저장하기
     public ResponseEntity<ApiResponse> updateUserGame() {
         return ResponseEntity.ok(ApiResponse.ok());
