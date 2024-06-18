6917a9a6faa6201fbc2f400ecb928a665be6c34f
최상혁 <shchoi3@enliple.com>
2024-06-18 15:59:02 +0900

    - 공통응답리스트 모델추가
    (cherry picked from commit 27b6e8755f488f349c64a36b7bf928661a77131a)

--- raising/src/main/java/com/mobcomms/raising/service/UserService.java
+++ raising/src/main/java/com/mobcomms/raising/service/UserService.java
@@ -1,4 +1,18 @@
 package com.mobcomms.raising.service;
 
+import com.mobcomms.raising.dto.UserReqDto;
+import com.mobcomms.raising.dto.UserResDto;
+import com.mobcomms.raising.entity.UserEntity;
+import com.mobcomms.raising.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserResDto readUser(String mediaUserKey) {
+        return UserResDto.of(userRepository.findByMediaUserKey());
+    }
 }
