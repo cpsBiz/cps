e85d5a4ac9e7f1135049bbebcb8cff62e528744b
mobcomms <mobcomms.app@enliple.com>
2024-06-27 12:58:06 +0900

    adPanel테스트

--- adPanel/src/main/java/com/mobcomms/adPanel/config/interceptor/InterceptorConfig.java
+++ adPanel/src/main/java/com/mobcomms/adPanel/config/interceptor/InterceptorConfig.java
@@ -4,6 +4,7 @@ import com.mobcomms.common.interceptor.PerformanceInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,8 +14,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorConfig implements WebMvcConfigurer  {
 
 	private final PerformanceInterceptor performanceInterceptor;
+	public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
 	@Override
 	public void addInterceptors (InterceptorRegistry registry) {
 		registry.addInterceptor(performanceInterceptor).addPathPatterns("/api/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("https://api.commsad.com") // 허용할 Origin 설정
+				.allowedMethods(ALLOWED_METHOD_NAMES.split(",")) // 허용할 HTTP 메서드 설정
+				.allowedHeaders("*") // 허용할 헤더 설정
+				.allowCredentials(true) // 쿠키 인증 정보 허용 여부 설정
+				.maxAge(3600); // Preflight 요청 결과를 캐싱할 시간 설정
 	}
 }
